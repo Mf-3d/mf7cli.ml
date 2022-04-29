99110f6fec87f407dcf83e053d9d700001260225
mf-3d <mf72cli120@gmail.com>
2022-04-29 16:30:24 +0900

    [fix] 狂ってるぞ

--- api/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const app = express();
-
-var server = app.listen(3000, () => {
-    console.log("Node.js is listening to PORT:" + server.address().port);
-});
-
-app.use(express.static('/src'));
